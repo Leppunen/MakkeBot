edcadbc458d1314da1cd0f8b32860fb7be63c119
Leppunen <niko@ivr.fi>
2020-01-11 15:18:32 +0200

    Update 1uapi.js

--- 1uapi.js
+++ 1uapi.js
@@ -1,14 +1,14 @@
 // secret stuff etc credentials
-const credentials = require('./config.js');
+const config = require('./config.js');
 
 const axios = require('axios');
 
 module.exports = async (url) => {
-    if (!credentials._1uApiKey) {
+    if (!config._1uApiKey) {
         return null;
     }
     try {
-        const {data} = await axios(`https://1u.fi/api/?key=${credentials._1uApiKey}&url=${url}`);
+        const {data} = await axios(`https://1u.fi/api/?key=${config._1uApiKey}&url=${url}`);
         if (!data.error) {
             return data.short;
         } else {
